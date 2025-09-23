58ed52d423972ba22f4a1a7b795f284d2883318f
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-23 10:20:50 +0200

    Create cuestiones.js

---
+++ cuestiones.js
@@ -0,0 +1,10 @@
+alert("Se reconoce el archivo JS");
+/*
+preguntas=new Array();
+respuestas=new Array();
+n=0;
+preguntas[n]='¿Cuál es la capital de España?';respuestas[n]='Madrid';n++;
+preguntas[n]='Mensaje emeregente Hola';respuestas[n]='alert("Hola")';n++;
+preguntas[n]='Pedir un número (variable a)';respuestas[n]='a=1*prompt("Dime un número")';n++;
+preguntas[n]='Preguntar cómo te llamas (variable nombre)';respuestas[n]='';n++;
+*/
