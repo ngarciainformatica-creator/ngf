da32e022d84a390e120faec4be001c804785403a
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-23 18:16:38 +0200

    Update cuestiones01.js

--- cuestiones01.js
+++ cuestiones01.js
@@ -1,8 +1,7 @@
-nombre="Natalio"; //Esto es una prueba
 preguntas=new Array();
 respuestas=new Array();
 n=0;
 preguntas[n]='¿Cuál es la capital de España?';respuestas[n]='Madrid';n++;
-preguntas[n]='Mensaje emeregente Hola';respuestas[n]='alert("Hola")';n++;
+preguntas[n]='Mensaje emergente Hola';respuestas[n]='alert("Hola")';n++;
 preguntas[n]='Pedir un número (variable a)';respuestas[n]='a=1*prompt("Dime un número")';n++;
-preguntas[n]='Preguntar cómo te llamas (variable nombre)';respuestas[n]='';n++;
+preguntas[n]='Preguntar cómo te llamas (variable nombre)';respuestas[n]='nombre=prompt("¿Cómo te llamas?")';n++;
