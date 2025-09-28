601c5ed7c673f7d9728fb4613dd84fdd0eb83d07
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-28 18:41:17 +0200

    Update cuestiones01.js

--- cyr1eso/cuestiones01.js
+++ cyr1eso/cuestiones01.js
@@ -15,11 +15,15 @@ preguntas[n]='Mostrar el valor de c';respuestas[n]='alert(c)';n++;
 preguntas[n]='Mensaje <b>Hasta luego</b>';respuestas[n]='alert("Hasta luego")';n++;
 
 
-preguntas[n]='<b><br><br>PROGRAMA 2<br><br></b>Pedir la nota de un examen en variable <b>a</b> (Dime primera nora)';respuestas[n]='a=1*prompt("Dime primera nota")';n++;
+preguntas[n]='<b><br><br>PROGRAMA 2<br><br></b>Pedir la nota de un examen en variable <b>a</b> (Dime primera nota)';respuestas[n]='a=1*prompt("Dime primera nota")';n++;
 preguntas[n]='Pedir la nota de otro examen en variable <b>b</b> (Dime segunda nota)';respuestas[n]='b=1*prompt("Dime segunda nota")';n++;
 preguntas[n]='Pedir la nota de otro examen más en variable <b>c</b> (Dime tercera nota)';respuestas[n]='c=1*prompt("Dime tercera nota")';n++;
 preguntas[n]='Hallar la media de las tres notas en una variable <b>media</b>';respuestas[n]='media=(a+b+c)/3';n++;
 preguntas[n]='Mostrar el valor de la media';respuestas[n]='alert(media)';n++;
+
+preguntas[n]='<b><br><br>PROGRAMA 3<br><br></b>Pedir un número x <b>a</b> (Dime un número)';respuestas[n]='x=1*prompt("Dime un número")';n++;
+preguntas[n]='Hallar el doble de x en una variable <b>y</b>';respuestas[n]='y=2*x';n++;
+preguntas[n]='Mostrar el valor de y';respuestas[n]='alert(y)';n++;
 
 /*
 preguntas[n]='';respuestas[n]='';n++;
