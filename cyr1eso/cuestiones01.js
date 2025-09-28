37cbc817dd7ff77465d91a97713e197bb78d18b0
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-28 18:36:29 +0200

    Update cuestiones01.js

--- cyr1eso/cuestiones01.js
+++ cyr1eso/cuestiones01.js
@@ -5,14 +5,21 @@ preguntas=new Array();
 respuestas=new Array();
 n=0;
 
-preguntas[n]='<b>EJERCICIOS DE CÓDIGOO JAVASCRIPT<br><br>PROGRAMA 1<br><br></b>Mensaje <b>Hola</b>';respuestas[n]='alert("Hola")';n++;
+/*
+preguntas[n]='<b>EJERCICIOS DE CÓDIGO JAVASCRIPT<br><br>PROGRAMA 1<br><br></b>Mensaje <b>Hola</b>';respuestas[n]='alert("Hola")';n++;
 preguntas[n]='Pedir un número en variable <b>a</b> (Dime un número)';respuestas[n]='a=1*prompt("Dime un número")';n++;
 preguntas[n]='Pedir otro número en variable <b>b</b> (Dime un número)';respuestas[n]='b=1*prompt("Dime otro número")';n++;
 preguntas[n]='Hallar la suma de los dos números en una variable <b>c</b>';respuestas[n]='c=a+b';n++;
 preguntas[n]='Mostrar el valor de c';respuestas[n]='alert(c)';n++;
+*/
 preguntas[n]='Mensaje <b>Hasta luego</b>';respuestas[n]='alert("Hasta luego")';n++;
 
 
+preguntas[n]='<b><br><br>PROGRAMA 2<br><br></b>Pedir la nota de un examen en variable <b>a</b> (Dime primera nora)';respuestas[n]='a=1*prompt("Dime primera nota")';n++;
+preguntas[n]='Pedir la nota de otro examen en variable <b>b</b> (Dime segunda nota)';respuestas[n]='b=1*prompt("Dime segunda nota")';n++;
+preguntas[n]='Pedir la nota de otro examen más en variable <b>c</b> (Dime tercera nota)';respuestas[n]='c=1*prompt("Dime tercera nota")';n++;
+preguntas[n]='Hallar la media de las tres notas en una variable <b>media</b>';respuestas[n]='media=(a+b+c)/3';n++;
+preguntas[n]='Mostrar el valor de la media';respuestas[n]='alert(media)';n++;
 
 /*
 preguntas[n]='';respuestas[n]='';n++;
