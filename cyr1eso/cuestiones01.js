522ec116f702d1ddff7acdac73db2c1162b1c9af
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-28 18:53:02 +0200

    Update cuestiones01.js

--- cyr1eso/cuestiones01.js
+++ cyr1eso/cuestiones01.js
@@ -7,7 +7,7 @@ n=0;
 
 preguntas[n]='<b>EJERCICIOS DE CÓDIGO JAVASCRIPT<br><br>PROGRAMA 1<br><br></b>Mensaje <b>Hola</b>';respuestas[n]='alert("Hola")';n++;
 preguntas[n]='Pedir un número en variable <b>a</b> (Dime un número)';respuestas[n]='a=1*prompt("Dime un número")';n++;
-preguntas[n]='Pedir otro número en variable <b>b</b> (Dime un número)';respuestas[n]='b=1*prompt("Dime otro número")';n++;
+preguntas[n]='Pedir otro número en variable <b>b</b> (Dime otro número)';respuestas[n]='b=1*prompt("Dime otro número")';n++;
 preguntas[n]='Hallar la suma de los dos números en una variable <b>c</b>';respuestas[n]='c=a+b';n++;
 preguntas[n]='Mostrar el valor de c';respuestas[n]='alert(c)';n++;
 preguntas[n]='Mensaje <b>Hasta luego</b>';respuestas[n]='alert("Hasta luego")';n++;
