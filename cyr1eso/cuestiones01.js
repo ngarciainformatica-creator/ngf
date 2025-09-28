4088c58a0ee3bac31206e27c14cf57c9f96547af
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-09-28 18:50:18 +0200

    Update cuestiones01.js

--- cyr1eso/cuestiones01.js
+++ cyr1eso/cuestiones01.js
@@ -11,16 +11,25 @@ preguntas[n]='Pedir otro número en variable <b>b</b> (Dime un número)';respues
 preguntas[n]='Hallar la suma de los dos números en una variable <b>c</b>';respuestas[n]='c=a+b';n++;
 preguntas[n]='Mostrar el valor de c';respuestas[n]='alert(c)';n++;
 preguntas[n]='Mensaje <b>Hasta luego</b>';respuestas[n]='alert("Hasta luego")';n++;
+preguntas[n]='¿Cuántos datos de entrada tiene este programa?';respuestas[n]='2';n++;
+preguntas[n]='¿Cuántos resultados (datos de salida) tiene este programa?';respuestas[n]='1';n++;
+preguntas[n]='Si ejecutas este programa varias veces (con distintos datos de entrada) ¿Da siempre el mismo resultado?';respuestas[n]='No';n++;
 
 preguntas[n]='<b><br><br>PROGRAMA 2<br><br></b>Pedir la nota de un examen en variable <b>a</b> (Dime primera nota)';respuestas[n]='a=1*prompt("Dime primera nota")';n++;
 preguntas[n]='Pedir la nota de otro examen en variable <b>b</b> (Dime segunda nota)';respuestas[n]='b=1*prompt("Dime segunda nota")';n++;
 preguntas[n]='Pedir la nota de otro examen más en variable <b>c</b> (Dime tercera nota)';respuestas[n]='c=1*prompt("Dime tercera nota")';n++;
 preguntas[n]='Hallar la media de las tres notas en una variable <b>media</b>';respuestas[n]='media=(a+b+c)/3';n++;
 preguntas[n]='Mostrar el valor de la media';respuestas[n]='alert(media)';n++;
+preguntas[n]='¿Cuántos datos de entrada tiene este programa?';respuestas[n]='3';n++;
+preguntas[n]='¿Cuántos resultados (datos de salida) tiene este programa?';respuestas[n]='1';n++;
 
 preguntas[n]='<b><br><br>PROGRAMA 3<br><br></b>Pedir un número x <b>a</b> (Dime un número)';respuestas[n]='x=1*prompt("Dime un número")';n++;
 preguntas[n]='Hallar el doble de x en una variable <b>y</b>';respuestas[n]='y=2*x';n++;
 preguntas[n]='Mostrar el valor de y';respuestas[n]='alert(y)';n++;
+preguntas[n]='Hallar el triple de x en una variable <b>z</b>';respuestas[n]='z=3*x';n++;
+preguntas[n]='Mostrar el valor de z';respuestas[n]='alert(z)';n++;
+preguntas[n]='¿Cuántos datos de entrada tiene este programa?';respuestas[n]='1';n++;
+preguntas[n]='¿Cuántos resultados (datos de salida) tiene este programa?';respuestas[n]='2';n++;
 
 /*
 preguntas[n]='';respuestas[n]='';n++;
