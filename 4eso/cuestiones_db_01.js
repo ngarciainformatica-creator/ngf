13c0126e7ebc9761a3aab89cfe6f80ce64500643
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-07 07:28:50 +0100

    Update and rename cuestiones06.js to cuestiones_db_01.js

--- 4eso/cuestiones_db_01.js
+++ 4eso/cuestiones_db_01.js
@@ -1,4 +1,5 @@
-relacion=6; // Es la relación 1 de ejercicios de tipo Decimal-Binario
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 1";
+// Es la relación 1 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
 
 preguntas=new Array();
 respuestas=new Array();
