8abbba4cdd213c8ac21ba4caf488d07cdd82e170
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-07 07:32:19 +0100

    Update cuestiones_db_01.js

--- 4eso/cuestiones_db_01.js
+++ 4eso/cuestiones_db_01.js
@@ -1,5 +1,6 @@
 descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 1";
 // Es la relación 1 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// Sistema decimal
 
 preguntas=new Array();
 respuestas=new Array();
