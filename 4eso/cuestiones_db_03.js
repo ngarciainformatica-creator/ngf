feb60b50d0f170274eded11df7c554123520c50b
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-09 20:45:30 +0100

    Create cuestiones_db_03.js

---
+++ 4eso/cuestiones_db_03.js
@@ -0,0 +1,8 @@
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 3";
+// Es la relación 3 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// Paso de decimal a binario y ejercicios relacionados
+
+preguntas=new Array();
+respuestas=new Array();
+n=0;
+
