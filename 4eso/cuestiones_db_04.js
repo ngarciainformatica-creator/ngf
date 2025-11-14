d7227d0c08ac907977ab58d92613bf04abb89b57
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-14 07:55:26 +0100

    Update and rename cuestiones_db_04.js to cuestiones_db_06.js

--- 4eso/cuestiones_db_04.js
+++
@@ -1,7 +0,0 @@
-descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 4";
-// Es la relación 4 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
-// ¿Rellenar este comentario?
-
-preguntas=new Array();
-respuestas=new Array();
-n=0;
---
+++ 4eso/cuestiones_db_06.js
@@ -0,0 +1,7 @@
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 6";
+// Es la relación 6 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// ¿Rellenar este comentario?
+
+preguntas=new Array();
+respuestas=new Array();
+n=0;
