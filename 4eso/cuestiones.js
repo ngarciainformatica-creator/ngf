2e6b408400aec92ee94e5adef732327dae0001ad
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-14 07:57:55 +0100

    Create cuestiones.js

---
+++ 4eso/cuestiones.js
@@ -0,0 +1,7 @@
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 2";
+// Es la relación 2 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// Sistema binario y hexadecimal. Concepto de byte.
+
+preguntas=new Array();
+respuestas=new Array();
+n=0;
