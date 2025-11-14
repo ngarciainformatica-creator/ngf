716446cf9cdbd4fa498778e8133e19da0b871ff2
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-14 07:58:54 +0100

    Create cuestiones_db_04.js

---
+++ 4eso/cuestiones_db_04.js
@@ -0,0 +1,7 @@
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 4";
+// Es la relación 4 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// Todos los valores hexadecimales de un byte
+
+preguntas=new Array();
+respuestas=new Array();
+n=0;
