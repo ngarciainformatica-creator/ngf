5258a81e202eefdda3404a3284486dbf537b8eb8
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-16 16:40:22 +0100

    Update cuestiones.js

--- curso_windows/atajos3/cuestiones.js
+++ curso_windows/atajos3/cuestiones.js
@@ -1,5 +1,5 @@
 descripcion="ATAJOS DE TECLADO EN WINDOWS - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 3";
-// Es la relación 2 de atajos Windows
+// Es la relación 3 de atajos Windows
 
 preguntas=new Array();
 respuestas=new Array();
