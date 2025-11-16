2c299ecbfb584245e774ca0c30e6b0cb791726de
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-16 17:12:57 +0100

    Update cuestiones.js

--- curso_windows/atajos2/cuestiones.js
+++ curso_windows/atajos2/cuestiones.js
@@ -4,3 +4,27 @@ descripcion="ATAJOS DE TECLADO EN WINDOWS - EJERCICIOS Y CUESTIONES - RELACIÓN 
 preguntas=new Array();
 respuestas=new Array();
 n=0;
+
+preguntas[n]='Alternar entre ventanas abiertas ';respuestas[n]='Alt + Tab';n++;
+preguntas[n]='Alternar entre ventanas de la misma aplicación (por ejemplo navegador) ';respuestas[n]='Control + Tab';n++;
+preguntas[n]='Mover ventana activa a escritorio virtual siguiente ';respuestas[n]='Windows + Control + FlechaDch';n++;
+preguntas[n]='Mover ventana activa a escritorio virtual anterior ';respuestas[n]='Windows + Control + FlechaIzq';n++;
+preguntas[n]='Crear nuevo escritorio virtual ';respuestas[n]='Windows + Control + D';n++;
+preguntas[n]='Cerrar escritorio virtual actual ';respuestas[n]='Windows + Control + F4';n++;
+preguntas[n]='Mostrar todos los escritorios virtuales ';respuestas[n]='Windows + Tab';n++;
+preguntas[n]='Minimizar ventana activa ';respuestas[n]='Windows + FlechaAbajo';n++;
+preguntas[n]='Maximizar ventana activa ';respuestas[n]='Windows + FlechaArriba';n++;
+preguntas[n]='Restaurar tamaño de ventana maximizada ';respuestas[n]='Windows + FlechaAbajo';n++;
+preguntas[n]='Ajustar ventana a la izquierda de la pantalla ';respuestas[n]='Windows + FlechaIzq';n++;
+preguntas[n]='Ajustar ventana a la derecha de la pantalla ';respuestas[n]='Windows + FlechaDch';n++;
+preguntas[n]='Ajustar ventana a cuadrante superior izquierdo ';respuestas[n]='Windows + FlechaArriba + FlechaIzq';n++;
+preguntas[n]='Ajustar ventana a cuadrante superior derecho ';respuestas[n]='Windows + FlechaArriba + FlechaDch';n++;
+preguntas[n]='Ajustar ventana a cuadrante inferior izquierdo ';respuestas[n]='Windows + FlechaAbajo + FlechaIzq';n++;
+preguntas[n]='Ajustar ventana a cuadrante inferior derecho ';respuestas[n]='Windows + FlechaAbajo + FlechaDch';n++;
+preguntas[n]='Mostrar escritorio (minimizar todo) ';respuestas[n]='Windows + D';n++;
+preguntas[n]='Cambiar enfoque entre paneles de una aplicación ';respuestas[n]='F6';n++;
+preguntas[n]='Cambiar entre documentos abiertos (misma aplicación, por ejemplo Excel o Word) ';respuestas[n]='Control + F6';n++;
+preguntas[n]='Cambiar a hoja siguiente (Excel) ';respuestas[n]='Control + AvPag';n++;
+preguntas[n]='Cambiar a hoja anterior (Excel) ';respuestas[n]='Control + RePag';n++;
+preguntas[n]='Activar el modo de pantalla completa en navegador web ';respuestas[n]='F11';n++;
+preguntas[n]='Actualizar página en navegador web ';respuestas[n]='F5';n++;
