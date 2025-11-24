7c0dfbbe0890178a45b6cd2947e127a9896d91ad
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-11-24 12:54:00 +0100

    Create cuestiones_db_07.js

---
+++ 4eso/cuestiones_db_07.js
@@ -0,0 +1,7 @@
+descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 7 - Representación de cadenas de texto";
+// Es la relación 7 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// ¿Rellenar este comentario?
+
+preguntas=new Array();
+respuestas=new Array();
+n=0;
