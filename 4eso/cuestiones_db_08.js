42fe1c9c9c746131fa9f2accedeb31283325e66b
ngarciainformatica-creator <ngarciainformatica@iespadresuarez.es>
2025-12-15 16:48:00 +0100

    Update cuestiones_db_08.js

--- 4eso/cuestiones_db_08.js
+++ 4eso/cuestiones_db_08.js
@@ -1,5 +1,5 @@
 descripcion="REPRESENTACIÓN INTERNA EN DISPOSITIVOS DIGITALES - EJERCICIOS Y CUESTIONES - RELACIÓN Nº 8 - Representación de imágenes (mapas de bits)";
-// Es la relación 7 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
+// Es la relación 8 de ejercicios de tipo Decimal-Binario, concepto de byte, múltiplos, etc.
 // ¿Rellenar este comentario?
 
 preguntas=new Array();
